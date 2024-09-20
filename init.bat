1893421c599087407d1d110a22bab8ba1c1985c5
Dan Adrian Mirea <damirea84@gmail.com>
2024-09-20 17:56:39 +0300

    remove binary and init.bat

--- init.bat
+++
@@ -1,7 +0,0 @@
-echo "# tokenize_string" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:danadrianmirea/tokenize_string.git
-git push -u origin main
